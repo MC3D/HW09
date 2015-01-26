284ff92db06baac86227f5b185e851cd1d1d1b45
MC3D <m.chapman.home@gmail.com>
2015-01-26 07:41:39 -0500

    adds filter

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-(function(){
+(function() {
   // safe mode
   'use strict';
 
-
-  // name corresponds to the template name (e.g. beagleResults)
+  // name corresponds to the template name (e.g. searchResults)
   function renderTemplate(name, data) {
     var $template = $('[data-template-name=' + name + ']').text();
     $.each(data, function(prop, value) {
@@ -16,13 +15,59 @@
 
   // references var beagle in data.js
   var results = beagle.results;
+  var $list = $('#container'); // targets div with id container in the html
 
-  $(document).ready(function(){ // runs the function on document ready
-    var $list = $('#beagle'); // targets div with id beagle in the html
+  $(document).ready(function() { // runs the function on document ready
+    // var $list = $('#container'); // targets div with id container in the html
 
-    results.forEach(function(item){ // for each item in results (references var results)
-      // renderTemplate references function renderTemplate above (link 5)
-      var beagleItems = renderTemplate('beagleResults',{ //beagleResults is the name of the template in the html file I want to render
+    filter(results);
+
+    // results.forEach(function(item) { // for each item in results (references var results)
+    //   // renderTemplate references function renderTemplate above (line 6)
+    //   var searchItems = renderTemplate('searchResults', { //searchResults is the name of the template in the html file I want to render
+    //     itemUrl: item.url,
+    //     itemImage: item.Images[0].url_170x135,
+    //     itemTitle: item.title,
+    //     itemShopUrl: item.Shop.url,
+    //     itemPrice: item.price,
+    //     itemCurrency: item.currency_code
+    //   });
+    //   $list.append(searchItems); // searchItems is set at the start of the forEach function
+    // });
+  });
+
+  // updates list based on filter selection
+  $('#results-filter').on('change', function(event) { // runs the function when the select option changes
+    if ($('#results-filter option:selected').text() === 'Lowest Price') {
+
+      var sortedResults = _.sortBy(results, function(item) { // sorts the results by price in ascending order
+        return Number(item.price);
+      });
+
+      filter(sortedResults);
+
+    } else if ($('#results-filter option:selected').text() === 'Highest Price') {
+      var sortedResults = _.sortBy(results, function(item) { // sorts the results by price in descending order
+        return Number(item.price);
+      }).reverse();
+
+      // passes sortedResults to function filter
+      filter(sortedResults);
+
+    } else if ($('#results-filter option:selected').text() === 'Relevancy') {
+      results = beagle.results;
+      filter(results);
+    }
+  });
+
+  function filter(results) {
+    // $('#container').empty(); // targets div with id container in the html
+    // var $list = $('#container');
+    $list.empty();
+
+    results.forEach(function(item) { // for each item in sortResults
+      // renderTemplate references function renderTemplate above (line 6)
+      var searchItems = renderTemplate('searchResults', { //searchResults is the name of the template in the html file I want to render
         itemUrl: item.url,
         itemImage: item.Images[0].url_170x135,
         itemTitle: item.title,
@@ -30,12 +75,13 @@
         itemPrice: item.price,
         itemCurrency: item.currency_code
       });
-      $list.append(beagleItems); // beagleItems is set at the start of the forEach function
+      $list.append(searchItems); // searchItems is set at the start of the forEach function
     });
-  });
+
+  }
 
   // THE ORIGINAL JAVASCRIPT COMMENTED OUT; NOW RENDERED THROUGH A TEMPLATE
-  // $('#beagle').append(
+  // $('#container').append(
   //   '<div class="container">'+
   //   '<div>'+
   //   '<a class="image_link" href="'+
